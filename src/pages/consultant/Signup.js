107d641d081f2b1cb949aa6cbee45c52b50e3e0e
Shalitha-Roshan <it21804274@my.sliit.lk>
2024-03-21 12:14:31 +0530

    Implemented the Login signup routes, Init the tailwind CSS and Changed the App.js page with Routes

---
+++ src/pages/consultant/Signup.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Form from "../../components/form";
+
+const Signup = () => {
+    return (
+        <div>
+            <Form/>
+        </div>
+    );
+};
+
+export default Signup;
